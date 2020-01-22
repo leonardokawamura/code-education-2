13beb067a0409a16d239831d76738459b4ed760f
leonardo <leonardokawamura@msn.com>
2020-01-22 11:11:14 -0300

    route model binding

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Models\Category;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
 
@@ -26,6 +27,11 @@ class RouteServiceProvider extends ServiceProvider
         //
 
         parent::boot();
+
+        Route::bind('category', function($value) {
+            $collection = Category::whereId($value)->orWhere('slug', $value)->get();
+            return $collection->first();
+        });
     }
 
     /**
