09533cc13e6a31238d4f4e362da6b8c2ac80a188
leonardo <leonardokawamura@msn.com>
2020-01-23 12:07:10 -0300

    listando categorias de produtos

--- app/Http/Controllers/Api/ProductCategoryController.php
+++ app/Http/Controllers/Api/ProductCategoryController.php
@@ -3,13 +3,14 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class ProductCategoryController extends Controller
 {
-    public function index()
+    public function index(Product $product)
     {
-        //
+        return $product->categories;
     }
     
     public function store(Request $request)
