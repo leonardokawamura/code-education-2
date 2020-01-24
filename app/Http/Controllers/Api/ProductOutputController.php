a057c22e4bf0bd62e536bfea57c09fa39f3ae0fe
leonardo <leonardokawamura@msn.com>
2020-01-24 17:10:00 -0300

    regra personalizada para validar estoque na saída

--- app/Http/Controllers/Api/ProductOutputController.php
+++ app/Http/Controllers/Api/ProductOutputController.php
@@ -3,23 +3,26 @@
 namespace App\Http\Controllers\Api;
 
 use App\Models\ProductOutput;
-use Illuminate\Http\Request;
+use App\Http\Resources\ProductOutputResource;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\ProductOutputRequest;
 
 class ProductOutputController extends Controller
 {
     public function index()
     {
-
+        $outputs = ProductOutput::with('product')->paginate();        
+        return ProductOutputResource::collection($outputs);
     }
 
-    public function store($request)
+    public function store(ProductOutputRequest $request)
     {
-        
+        $output = ProductOutput::create($request->all());        
+        return new ProductOutputResource($output);
     }
 
-    public function show($input)
+    public function show(ProductOutput $output)
     {
-        
+        return new ProductOutputResource($output);
     }
 }
