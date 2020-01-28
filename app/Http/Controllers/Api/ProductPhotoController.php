89840f82f629c187812ab895d070be26f0fa442d
leonardo <leonardokawamura@msn.com>
2020-01-28 17:25:29 -0300

    brincando com os conceitos de exclusão lógica

--- app/Http/Controllers/Api/ProductPhotoController.php
+++ app/Http/Controllers/Api/ProductPhotoController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers\Api;
 
 use App\Models\Product;
 use App\Models\ProductPhoto;
-use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\ProductPhotoRequest;
 use App\Http\Resources\ProductPhotoCollection;
