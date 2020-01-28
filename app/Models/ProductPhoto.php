579a456d76ca1751ea2dbd93391d2975d8043055
leonardo <leonardokawamura@msn.com>
2020-01-28 14:45:28 -0300

    database transaction e tratamento de exceção

--- app/Models/ProductPhoto.php
+++ app/Models/ProductPhoto.php
@@ -2,6 +2,8 @@
 
 namespace App\Models;
 
+use DB;
+use File;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
 
@@ -21,9 +23,27 @@ class ProductPhoto extends Model
 
     public static function createWithPhotosFiles(int $productId, array $files)
     {
-        self::uploadFiles($productId, $files);
-        $photos = self::createPhotosModels($productId, $files);
-        return new Collection($photos);
+        try {
+            self::uploadFiles($productId, $files);
+            DB::beginTransaction();
+            $photos = self::createPhotosModels($productId, $files);
+            DB::commit();
+            return new Collection($photos);
+        } catch (\Exception $e) {
+            self::deleteFiles($productId, $files);
+            DB::rollBack();
+        }    
+    }
+    
+    public static function deleteFiles(int $productId, array $files)
+    {
+        foreach($files as $file) {
+            $path = self::photosPath($productId);
+            $photoPath = "{$path}/{$file->hashName()}";
+            if (file_exists($photoPath)) {
+                File::delete($photoPath);
+            }
+        }
     }
 
     public static function uploadFiles(int $productId, array $files)
