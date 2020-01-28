92e5a16f22c0c45d43996de1d1f07e769d77d469
leonardo <leonardokawamura@msn.com>
2020-01-28 16:39:48 -0300

    iniciando com exclusão lógica

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,11 +4,13 @@ namespace App\Models;
 
 use Cviebrock\EloquentSluggable\Sluggable;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Product extends Model
 {
-    use Sluggable;
+    use Sluggable, SoftDeletes;
     
+    protected $dates = ['deleted_at'];
     protected $fillable = ['name', 'description', 'price', 'active'];
 
     public function sluggable()
