7a3ed0eb77804857e95efba664373de6a7d1fc53
leonardo <leonardokawamura@msn.com>
2020-01-30 14:37:31 -0300

    JWT Starter - Payload

--- jwt_test.php
+++ jwt_test.php
@@ -1,9 +1,9 @@
 <?php
 
 $header = [
-        'alg' => 'HS256',
-        'typ' => 'JWT'
-    ];
+    'alg' => 'HS256',
+    'typ' => 'JWT'
+];
 
 $header_json = json_encode($header);
 $header_base64 = base64_encode($header_json);
@@ -11,3 +11,17 @@ $header_base64 = base64_encode($header_json);
 echo 'Cabeçalho JSON: ' . $header_json;
 echo "\n";
 echo 'Cabeçalho JWT: ' . $header_base64;
+echo "\n";
+
+$payload = [
+    'first_name' => 'leonardo',
+    'last_name' => 'kawamura',
+    'email' => 'leonardo@garagmeidia.com.br'
+];
+
+$payload_json = json_encode($payload);
+$payload_base64 = base64_encode($payload_json);
+
+echo 'Cabeçalho JSON: ' . $payload_json;
+echo "\n";
+echo 'Cabeçalho JWT: ' . $payload_base64;
