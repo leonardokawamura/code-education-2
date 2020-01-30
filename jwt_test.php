403797d3969cd9cf53ade752842e40823bd8ea1e
leonardo <leonardokawamura@msn.com>
2020-01-30 15:01:30 -0300

    JWT Starter - Signature

--- jwt_test.php
+++ jwt_test.php
@@ -11,7 +11,7 @@ $header_base64 = base64_encode($header_json);
 echo 'Cabeçalho JSON: ' . $header_json;
 echo "\n";
 echo 'Cabeçalho JWT: ' . $header_base64;
-echo "\n";
+echo "\n\n";
 
 $payload = [
     'first_name' => 'leonardo',
@@ -25,3 +25,16 @@ $payload_base64 = base64_encode($payload_json);
 echo 'Cabeçalho JSON: ' . $payload_json;
 echo "\n";
 echo 'Cabeçalho JWT: ' . $payload_base64;
+echo "\n\n";
+
+$key = 'alskdfjals;dfjkalskfjlaksfj0w9rusfjksfljlkf';
+$signature = hash_hmac('sha256', "$header_base64.$payload_base64", $key, true); 
+$signature_base64 = base64_encode($signature);
+
+echo "Signature: $signature";
+echo "\n";
+echo 'Signature JWT: ' . $signature_base64;
+echo "\n\n";
+
+$token = "$header_base64.$payload_base64.$signature_base64";
+echo "TOKEN: $token";
