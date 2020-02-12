7541c61ee941a118317ec3d16eb3000b4ab96a06
leonardo <leonardokawamura@msn.com>
2020-02-12 15:42:06 -0300

    criando cadastro de categorias

--- _angular-app/src/app/components/pages/category/category-list/category-list.component.ts
+++ _angular-app/src/app/components/pages/category/category-list/category-list.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+declare let $;
+
 @Component({
   selector: 'app-category-list',
   templateUrl: './category-list.component.html',
@@ -10,16 +12,43 @@ export class CategoryListComponent implements OnInit {
 
   categories = [];
 
+  category = {
+    name: ''
+  };
+
   constructor(private http: HttpClient) { }
 
   ngOnInit() {
+    this.getCategories();
+  }
+
+  submit() {
     const token = window.localStorage.getItem('token');
-    this.http.get<{data: Array<any>}>('http://localhost/projects/code-education/public/api/categories', {
+    this.http
+      .post('http://localhost/projects/code-education/public/api/categories', this.category, {
       headers: {
         'Authorization' : 'Bearer ' + token
       }
     })
-      .subscribe(response => this.categories = response.data);
+      .subscribe((category) => {
+        console.log(category);
+        this.getCategories();
+        $('#exampleModal').modal('hide');        
+      });
+  }
+
+  getCategories() {
+    const token = window.localStorage.getItem('token');
+    this.http
+      .get<{data: Array<any>}>('http://localhost/projects/code-education/public/api/categories', {
+      headers: {
+        'Authorization' : 'Bearer ' + token
+      }
+    })
+      .subscribe(response => {
+        response.data[0].active = false;
+        this.categories = response.data;
+      });
   }
 
 }
