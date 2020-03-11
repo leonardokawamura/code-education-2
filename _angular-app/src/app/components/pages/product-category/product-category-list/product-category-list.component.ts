869628b947714545b8d74466a759dd0f5557a76a
leonardo kawamura <leonardokawamura@gmail.com>
2020-03-11 10:37:02 -0300

    criando campo select para listar categorias

--- _angular-app/src/app/components/pages/product-category/product-category-list/product-category-list.component.ts
+++ _angular-app/src/app/components/pages/product-category/product-category-list/product-category-list.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ProductHttpService } from 'src/app/services/http/product-http.service';
-import { Product, ProductCategory } from 'src/app/model';
+import { Product, ProductCategory, Category } from 'src/app/model';
 import { ProductCategoryHttpService } from 'src/app/services/http/product-category-http.service';
+import { CategoryHttpService } from 'src/app/services/http/category-http.service';
 
 @Component({
   selector: 'app-product-category-list',
@@ -14,17 +15,33 @@ export class ProductCategoryListComponent implements OnInit {
   productId: number;
   product: Product = null;
   productCategory: ProductCategory = null;
+  categories: Array<Category> = [];
+  categoriesId: Array<number> = [];
 
   constructor(private route: ActivatedRoute, 
               private productHttp: ProductHttpService,
-              private productCategoryHttp: ProductCategoryHttpService) { }
+              private productCategoryHttp: ProductCategoryHttpService,
+              private categoryHttp: CategoryHttpService) { }
 
   ngOnInit() {
+    this.getCategories();
     this.route.params.subscribe(params => {
       this.productId = params.product;
       this.getProduct();
       this.getProductCategory();
     });
+  }
+
+  change() {
+    console.log(this.categoriesId);
+  }
+
+  getCategories() {
+    this.categoryHttp
+      .list(1)
+      .subscribe(response => {
+        this.categories = response.data;
+      })
   }
 
   getProduct() {
