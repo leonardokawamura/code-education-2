dd259d5227f908e616851fe8f23a152558cb539c
leonardo kawamura <leonardokawamura@gmail.com>
2020-03-12 17:24:58 -0300

    testando renovacao do  token

--- _angular-app/src/app/services/refresh-token-interceptor.service.ts
+++ _angular-app/src/app/services/refresh-token-interceptor.service.ts
@@ -1,30 +1,43 @@
 import { Injectable } from '@angular/core';
-import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HttpResponse } from '@angular/common/http';
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HttpResponse, HttpErrorResponse } from '@angular/common/http';
 import { Observable } from 'rxjs/internal/Observable';
 import { tap } from 'rxjs/operators';
+import { AuthService } from './auth.service';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RefreshTokenInterceptorService implements HttpInterceptor {
 
-  constructor() { }
+  constructor(private authService: AuthService, private router: Router) { }
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     return next
       .handle(req)
       .pipe(
         tap((event: HttpEvent<any>) => {
-          console.log(event);
           this.setNewTokenIfResponseValid(event);
+        }, (eventError: HttpEvent<any>) => {
+          this.redirectToLoginIfUnaunthenticated(eventError);
         })
       )
+  }
+
+  private redirectToLoginIfUnaunthenticated(eventError: HttpEvent<any>) {
+    if(eventError instanceof HttpErrorResponse && eventError.status == 401) {
+      this.authService.setToken(null);
+      this.router.navigate(['login']);
+    }
   }
 
   private setNewTokenIfResponseValid(event: HttpEvent<any>) {
     if(event instanceof HttpResponse) {
       const authorizathionHeader = event.headers.get('authorization');
-      console.log(authorizathionHeader);     
+      if(authorizathionHeader) {
+        const token = authorizathionHeader.split(' ')[1];
+        this.authService.setToken(token);
+      }    
     }
   }
 }
