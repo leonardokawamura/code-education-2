c13b1a466956ec22a84d078ce2aa6464744be340
leonardo kawamura <leonardokawamura@gmail.com>
2020-04-08 11:28:38 -0300

    redirecionamento usuario para a area  principal

--- _whatsapp/src/pages/login-phone-number/login-phone-number.ts
+++ _whatsapp/src/pages/login-phone-number/login-phone-number.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { FirebaseAuthProvider } from '../../providers/auth/firebase-auth';
 import { AuthProvider } from '../../providers/auth/auth';
+import { MainPage } from '../main/main';
 
 /**
  * Generated class for the LoginPhoneNumberPage page.
@@ -24,13 +25,24 @@ export class LoginPhoneNumberPage {
   }
 
   ionViewDidLoad() {
-    this.authService.login().subscribe(token => console.log(token));
-    /* this.firebaseAuth.getToken().then(token => {
-      console.log(token);
-    }, error => {
-      console.log(error);
-    });
-    this.firebaseAuth.makePhoneNumberForm('#firebase-ui'); */
+    this.firebaseAuth.firebase.auth().onAuthStateChanged(user => {
+      if(user) {
+        this.authService.login().subscribe(token => {
+          this.redirectToMainPage();
+        }, responseError => {
+
+        }); 
+      }
+    });       
+    this.firebaseAuth.makePhoneNumberForm('#firebase-ui');
+  }
+
+  redirectToMainPage() {
+    this.navCtrl.setRoot(MainPage);
+  }
+
+  redirectToCustomerCreatePage() {
+    
   }
 
 }
