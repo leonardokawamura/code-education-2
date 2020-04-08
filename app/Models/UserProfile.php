0952a221dcb23a913e1e8a75d5a7709858c1ae90
leonardo kawamura <leonardokawamura@gmail.com>
2020-04-08 14:52:53 -0300

    finalizando metodo para criacao d e cliente

--- app/Models/UserProfile.php
+++ app/Models/UserProfile.php
@@ -1,9 +1,10 @@
 <?php
-
+declare(strict_types = 1);
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\File;
 
 class UserProfile extends Model
 {
@@ -14,6 +15,20 @@ class UserProfile extends Model
     const USER_PHOTO_PATH = self::BASE_PATH . '/' . self::DIR_USER_PHOTO;
 
     protected $fillable = ['photo', 'phone_number'];
+
+    public function saveProfile(User $user, array $data): UserProfile
+    {
+        $data['photo'] = UserProfile::getPhotoHashName($data['photo']);
+        $user->profile->fill($data)->save();
+        return $user->profile;
+    }
+    
+    private static function getPhotoHashName(UploadedFile $photo = null)
+    {
+        return $photo ? $photo->hashName() : null;
+    }
+    
+    
 
     public static function photoPath()
     {
@@ -29,6 +44,20 @@ class UserProfile extends Model
         $dir = self::photoDir();
         $photo->store($dir, ['disk' => 'public']);
     }
+
+    public function deleteFile($photo = null)
+    {
+        if(!$photo) {
+            return;
+        }
+        $path = self::photoPath();
+        $photoPath = "{$path}/{$photo->hashName()}";
+        if(file_exists($photoPath)) {
+            File::delete($photoPath);
+        }
+    }
+    
+    
 
     public static function photoDir()
     {
