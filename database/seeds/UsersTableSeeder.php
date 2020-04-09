55581d104afc96b7d8c772d0e030e1eddc6e6d66
leonardo kawamura <leonardokawamura@gmail.com>
2020-04-09 17:44:28 -0300

    melhoria na seeder de usuario

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -1,7 +1,11 @@
 <?php
 
 use App\Models\User;
+use App\Models\UserProfile;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Seeder;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\File;
 
 class UsersTableSeeder extends Seeder
 {
@@ -12,25 +16,45 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
+        File::deleteDirectory(UserProfile::photoPath(), true);
+
         factory(User::class, 1)
             ->create([
                 'email' => 'admin@user.com'
             ])
-            ->each(function ($user) {
-                $user->profile->phone_number = '+16505551234';
-                $user->profile->save();
+            ->each(function ($user) {    
+                Model::reguard();            
+                $user->updateWithProfile([
+                    'phone_number' => '+16505551234',
+                    'photo' => $this->getAdminPhoto()
+                ]);
+                Model::unguard();
             });
 
         factory(User::class, 1)
             ->create([
                 'email' => 'customer@user.com',
                 'role' => User::ROLE_CUSTOMER
-            ]);
+            ])
+            ->each(function ($user) {
+                Model::reguard();
+                $user->updateWithProfile([
+                    'phone_number' => '+16505551235'
+                ]);
+                Model::unguard();
+            });
 
         factory(User::class, 50)
             ->create([
                 'role' => User::ROLE_CUSTOMER   
             ]);  
     }    
+
+    public function getAdminPhoto()
+    {
+        return new UploadedFile(storage_path('app/faker/users/pp.jpg'), str_random(16) . '.jpg');
+    }
+    
+    
     
 }
