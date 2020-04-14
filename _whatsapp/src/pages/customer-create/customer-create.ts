953fb1fcfa9eb894756767adda2b10f6f8a55652
leonardo kawamura <leonardokawamura@gmail.com>
2020-04-13 21:00:21 -0300

    criando cliente pelo ionic

--- _whatsapp/src/pages/customer-create/customer-create.ts
+++ _whatsapp/src/pages/customer-create/customer-create.ts
@@ -1,6 +1,7 @@
 import { Component, ViewChild } from '@angular/core';
 import { IonicPage, NavController, NavParams, TextInput } from 'ionic-angular';
 import { FormBuilder, Validators, FormGroup } from '@angular/forms';
+import { CustomerHttpProvider } from '../../providers/http/customer-http';
 
 /**
  * Generated class for the CustomerCreatePage page.
@@ -22,10 +23,12 @@ export class CustomerCreatePage {
 
   constructor(public navCtrl: NavController, 
               public navParams: NavParams,
-              private formBuilder: FormBuilder) {
+              private formBuilder: FormBuilder,
+              private customerHttp: CustomerHttpProvider) {
     this.form = this.formBuilder.group({
-      name: ['', Validators.required, Validators.maxLength(255)],
-      email: ['', Validators.required, Validators.email, Validators.maxLength(255)]
+      name: ['', [Validators.required, Validators.maxLength(255)]],
+      email: ['', [Validators.required, Validators.email, Validators.maxLength(255)]],
+      photo: null
     });
   }
 
@@ -34,7 +37,11 @@ export class CustomerCreatePage {
   }
 
   submit() {
-    
+    this.customerHttp
+      .create(this.form.value)
+      .subscribe(() => {
+        console.log('cliente foi criado');
+      })
   }
 
   selectPhoto() {
@@ -42,5 +49,13 @@ export class CustomerCreatePage {
     const inputFile = nativeElement.querySelector('input');
     inputFile.click();
   }
+
+  onChoosePhoto(files: FileList) {
+    if(!files.length) {
+      return;
+    }
+    this.form.get('photo').setValue(files[0]);
+  }
+
 
 }
