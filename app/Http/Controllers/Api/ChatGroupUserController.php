bef29af0ab231748df07808d50e93494f338511b
leonardo kawamura <leonardokawamura@gmail.com>
2020-04-20 12:24:57 -0300

    listando membros de um grupo parte1

---
+++ app/Http/Controllers/Api/ChatGroupUserController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\ChatGroupUserRequest;
+use App\Http\Resources\ChatGroupUserResource;
+use App\Models\ChatGroup;
+use App\Models\User;
+
+class ChatGroupUserController extends Controller
+{
+    public function index(ChatGroup $chat_group)
+    {
+        return new ChatGroupUserResource($chat_group);
+    }
+
+    public function store(ChatGroupUserRequest $request, ChatGroup $chat_group)
+    {
+        $chat_group->users()->attach($request->users);
+        $users = User::whereIn('id', $request->users)->get();
+        return response()->json(new ChatGroupUserResource($chat_group, $users), 201);
+    }
+
+    public function destroy(ChatGroup $chat_group, User $user)
+    {
+        $chat_group->users()->detach($user->id);
+        return response()->json([], 204);
+    }
+}
---
+++ app/Http/Resources/ChatGroupUserResource.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Pagination\AbstractPaginator;
+
+class ChatGroupUserResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        self::withoutWrapping();
+        return $this->makeArray($request);
+    }
+
+    protected function getChatGroup()
+    {
+        $chat_group = $this->resource;
+        $chat_group->users_count = $this->resource->users()->count();
+        return $chat_group;
+    }
+
+    protected function makeArray($request)
+    {
+        $chatGroup = $this->getChatGroup();
+        $array = [
+            'data' => [
+                'chat_group' => new ChatGroupResource($chatGroup)
+            ]
+        ];
+        $userResponse = $this->getUserResponse($request);
+        if($userResponse instanceof JsonResponse) {
+            $data = $userResponse->getData();
+            $array['data']['users'] = $data->data;
+            $array['links'] = $data->links;
+            $array['meta'] = $data->meta;
+        } else {
+            $array['data']['users'] = $userResponse;
+        }
+        return $array;
+    }
+
+    protected function getUserResponse($request)
+    {
+        $users = $this->users ? $this->users : $this->resource->users()->paginate(1);
+        return $users instanceof AbstractPaginator ? 
+            UserResource::collection($users)->toResponse($request) : 
+            UserResource::collection($users);
+    }
+    
+    
+    
+    
+    
+    
+}
