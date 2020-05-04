809c29e907a7737269b5616467f742e0f5e5cb24
leonardo kawamura <leonardokawamura@gmail.com>
2020-05-04 19:39:42 -0300

    envio mensagem de imagem pelo ionic

--- _whatsapp/src/pages/chat-messages/chat-footer/chat-footer.ts
+++ _whatsapp/src/pages/chat-messages/chat-footer/chat-footer.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { ChatMessageHttpProvider } from '../../../providers/http/chat-message-http';
+import { TextInput } from 'ionic-angular';
 
 /**
  * Generated class for the ChatFooterComponent component.
@@ -16,11 +17,31 @@ export class ChatFooterComponent {
   text: string = '';
   messageType = 'text';
 
+  @ViewChild('inputFileImage') InputFileImage: TextInput;
+
   constructor(private chatMessageHttp: ChatMessageHttpProvider) {}
 
-  sendMessage() {
-    this.chatMessageHttp.create(1, {type: this.messageType, content: this.text})
+  sendMessageText() {
+    this.sendMessage({content: this.text, type: 'text'});
+  }
+
+  sendMessageImage(files: FileList) {
+    if(!files.length) {
+      return;
+    }
+    this.sendMessage({content: files[0], type: 'image'});
+  }
+
+  sendMessage(data: {content, type}) {
+    this.chatMessageHttp
+      .create(1, data)
       .subscribe(() => {console.log('enviou')});
+  }
+
+  selectImage() {
+    const nativeElement: HTMLElement = this.InputFileImage.getElementRef().nativeElement;
+    const inputFile = nativeElement.querySelector('input');
+    inputFile.click();
   }
 
 }
