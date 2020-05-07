c1701e1b31d24d9043ee492676e1c4becdf04d9d
leonardo kawamura <leonardokawamura@gmail.com>
2020-05-07 19:25:17 -0300

    montando cronometro

--- _whatsapp/src/pages/chat-messages/chat-footer/chat-footer.ts
+++ _whatsapp/src/pages/chat-messages/chat-footer/chat-footer.ts
@@ -1,6 +1,7 @@
 import { Component, ViewChild } from '@angular/core';
 import { ChatMessageHttpProvider } from '../../../providers/http/chat-message-http';
 import { TextInput } from 'ionic-angular';
+import Timer from 'easytimer.js/dist/easytimer.min';
 
 /**
  * Generated class for the ChatFooterComponent component.
@@ -16,10 +17,28 @@ export class ChatFooterComponent {
 
   text: string = '';
   messageType = 'text';
+  timer = new Timer();
 
   @ViewChild('inputFileImage') InputFileImage: TextInput;
 
   constructor(private chatMessageHttp: ChatMessageHttpProvider) {}
+
+  holdAudioButton() {
+    this.timer.start({precision: 'seconds'});
+    this.timer.addEventListener('secondsUpdated', (e) => {
+      const time = this.getMinutesSeconds();
+      this.text = `${time} Gravando...`;
+    })
+  } 
+
+  private getMinutesSeconds() {
+    return this.timer.getTimeValues().toString().substring(3);
+  }
+
+  releaseAudioButton() {
+    this.timer.stop();
+    this.text = '';
+  }
 
   sendMessageText() {
     this.sendMessage({content: this.text, type: 'text'});
