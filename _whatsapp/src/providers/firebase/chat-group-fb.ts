72e5358c9181b8c01ab35b3999e33d592dd1502b
leonardo kawamura <leonardokawamura@gmail.com>
2020-05-11 15:08:56 -0300

    delimitando se  usuario e membro do grupo

--- _whatsapp/src/providers/firebase/chat-group-fb.ts
+++ _whatsapp/src/providers/firebase/chat-group-fb.ts
@@ -2,7 +2,8 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { FirebaseAuthProvider } from '../auth/firebase-auth';
 import { Observable } from 'rxjs/Observable';
-import { ChatGroup } from 'app/model';
+import { ChatGroup, Role } from '../../app/model';
+import { AuthProvider } from '../../providers/auth/auth';
 
 /*
   Generated class for the ChatGroupFbProvider provider.
@@ -15,7 +16,8 @@ export class ChatGroupFbProvider {
 
   database;
 
-  constructor(private firebaseAuth: FirebaseAuthProvider) {
+  constructor(private firebaseAuth: FirebaseAuthProvider,
+              private auth: AuthProvider) {
     this.database = this.firebaseAuth.firebase.database();
   }
 
@@ -26,10 +28,25 @@ export class ChatGroupFbProvider {
         const groupsKey = Object.keys(groupsRaw);
         const groups = [];
         for(const key of groupsKey) {
+          groupsRaw[key].is_member = this.getMember(groupsRaw[key]);
           groups.push(groupsRaw[key]);
         }
         observer.next(groups);
       }, error => console.log(error));
+    });
+  }
+
+  getMember(group: ChatGroup): Observable<boolean> {
+    return Observable.create(observer => {
+      if(this.auth.me.role === Role.SELLER) {
+        observer.next(true);
+        return;
+      }
+      this.database
+        .ref(`chat_groups_users/${group.id}/${this.auth.me.profile.firebase_uid}`)
+        .on('value', data => {
+          return data.exists() ? observer.next(true) : observer.next(false);
+        });
     });
   }
 
