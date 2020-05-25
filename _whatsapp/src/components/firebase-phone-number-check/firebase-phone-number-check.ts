35a1dbcbe5818d7c2e1dee56ca3068a53b5219f4
leonardo kawamura <leonardokawamura@gmail.com>
2020-05-25 10:45:24 -0300

    testando captura do verificationid

--- _whatsapp/src/components/firebase-phone-number-check/firebase-phone-number-check.ts
+++ _whatsapp/src/components/firebase-phone-number-check/firebase-phone-number-check.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+import { Platform } from 'ionic-angular';
+declare const cordova;
 /**
  * Generated class for the FirebasePhoneNumberCheckComponent component.
  *
@@ -15,12 +16,17 @@ export class FirebasePhoneNumberCheckComponent {
   countryCode = "55";
   verificationId = "";
 
-  constructor() {
+  constructor(private platform: Platform) {
     
   }
 
   verifyPhoneNumber() {
-    this.verificationId = '111111';
+    this.platform.ready().then(() => {
+      cordova.plugins.firebase.auth.verifyPhoneNumber('+16505551234', 30000)
+        .then((verificationId) => {
+          console.log(verificationId)
+        }, (error) => console.log(error)); 
+    });
   }
 
   cancel() {
