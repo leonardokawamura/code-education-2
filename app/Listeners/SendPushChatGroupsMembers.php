0c8fa4da3ade3271a459cd8a4b87aca51c2a851c
leonardo kawamura <leonardokawamura@gmail.com>
2020-06-03 10:51:20 -0300

    criando metodo para pegar tokens dos membros de um grupo

--- app/Listeners/SendPushChatGroupsMembers.php
+++ app/Listeners/SendPushChatGroupsMembers.php
@@ -1,13 +1,14 @@
 <?php
-
+declare(strict_types = 1);
 namespace App\Listeners;
 
 use App\Events\ChatMessageSent;
-use Illuminate\Queue\InteractsWithQueue;
-use Illuminate\Contracts\Queue\ShouldQueue;
 
 class SendPushChatGroupsMembers
 {
+    /** @var ChatMessageSent */
+    private $event;
+
     /**
      * Create the event listener.
      *
@@ -26,6 +27,44 @@ class SendPushChatGroupsMembers
      */
     public function handle(ChatMessageSent $event)
     {
-        //
+        $this->event = $event;
     }
+
+    private function getTokens(): array
+    {
+        $membersTokens = $this->getMembersTokens();
+        $sellersTokens = $this->getSellersTokens();
+        return array_merge($membersTokens, $sellersTokens);
+    }
+
+    private function getMembersTokens(): array
+    {
+        $chatGroup = $this->event->getChatGroup();
+        $from = $this->event->getFrom();
+
+        /** @var Collection $users */
+        $users = $chatGroup
+            ->users()
+            ->whereHas('profile', function($query) use($from) {
+                $query
+                    ->whereNotNull('device_token')
+                    ->whereNotIn('id', [$from->profile->id]);
+            })->get();
+
+        /** @var \Illuminate\Support\Collection $membersTokensCollection */
+        $membersTokensCollection = $users->map(function ($user) {
+            return $user->profile->device_token;
+        });
+        
+        return $membersTokensCollection->toArray();
+    }
+
+    private function getSellersTokens(): array
+    {
+        return array();
+    }
+    
+    
+    
+    
 }
