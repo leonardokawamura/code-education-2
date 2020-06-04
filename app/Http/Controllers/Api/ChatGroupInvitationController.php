883ae9cd0e5b380158775b49c5e52a8db41a453e
leonardo kawamura <leonardokawamura@gmail.com>
2020-06-04 19:05:55 -0300

    criando model para convites de grupo

---
+++ app/Http/Controllers/Api/ChatGroupInvitationController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\ChatGroupInvitation;
+use Illuminate\Http\Request;
+
+class ChatGroupInvitationController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\ChatGroupInvitation  $chatGroupInvitation
+     * @return \Illuminate\Http\Response
+     */
+    public function show(ChatGroupInvitation $chatGroupInvitation)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\ChatGroupInvitation  $chatGroupInvitation
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(ChatGroupInvitation $chatGroupInvitation)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\ChatGroupInvitation  $chatGroupInvitation
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, ChatGroupInvitation $chatGroupInvitation)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\ChatGroupInvitation  $chatGroupInvitation
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(ChatGroupInvitation $chatGroupInvitation)
+    {
+        //
+    }
+}
---
+++ app/Models/ChatGroupInvitation.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Model;
+
+class ChatGroupInvitation extends Model
+{
+    protected $fillable = ['total', 'expires_at', 'group_id'];
+    protected $dates = ['expires_at', 'deleted_at'];
+
+    public function hasInvitation()
+    {
+        return $this->remaining > 0 && (!$this->expires_at or (new Carbon())->lessThanOrEqualTo("{$this->expires_at} 23:59:59"));
+    }
+
+    public function group()
+    {
+        return $this->belongsTo(ChatGroup::class, 'group_id');
+    }   
+    
+}
