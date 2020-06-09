eccd5d7311fae5dcac9c25bb67bf34cbfeb93971
leonardo kawamura <leonardokawamura@gmail.com>
2020-06-09 18:25:00 -0300

    organizando captura de push notification no ionic

--- _whatsapp/src/providers/push-notification/push-notification.ts
+++ _whatsapp/src/providers/push-notification/push-notification.ts
@@ -3,6 +3,15 @@ import { Injectable } from '@angular/core';
 import { FirebaseMessaging } from '@ionic-native/firebase-messaging';
 import { Platform } from 'ionic-angular';
 import { UserProfileHttp } from '../http/user-profile-http';
+import { merge } from 'rxjs/observable/merge';
+import { map } from 'rxjs/operators';
+import { Observable } from 'rxjs/Observable';
+import { ObserveOnOperator } from 'rxjs/operators/observeOn';
+
+enum NotificationType {
+  CHAT_GROUP_SUBSCRIBE = '1',
+  NEW_MESSAGE = '2'
+};
 
 /*
   Generated class for the PushNotificationProvider provider.
@@ -10,8 +19,14 @@ import { UserProfileHttp } from '../http/user-profile-http';
   See https://angular.io/guide/dependency-injection for more info on providers
   and Angular DI.
 */
+
 @Injectable()
 export class PushNotificationProvider {
+
+  private notification = merge<{background: boolean, data: any}>(
+    this.fcm.onBackgroundMessage().pipe(map(data => ({background: true, data}))),
+    this.fcm.onMessage().pipe(map(data => ({background: true, data})))
+  );
 
   constructor(private fcm: FirebaseMessaging,
               private platform: Platform,
@@ -35,6 +50,26 @@ export class PushNotificationProvider {
         this.userProfileHttp.update({device_token: token})
           .subscribe(() => {console.log('token registrado')})
       })
+  }
+
+  onChatGroupSubscribe(): Observable<{background: boolean, data: any}> {
+    return Observable.create(observer => {
+      this.notification.subscribe(data => {
+        if(data.data.type === NotificationType.CHAT_GROUP_SUBSCRIBE) {
+          observer.next(data);
+        }
+      })
+    });
+  }
+
+  onNewMesaage() {
+    return Observable.create(observer => {
+      this.notification.subscribe(data => {
+        if(data.data.type === NotificationType.NEW_MESSAGE) {
+          observer.next(data);
+        }
+      })
+    });
   }
 
 }
