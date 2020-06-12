01801943b5df8ba66c7d12ec1167196f4cef31d5
leonardo kawamura <leonardokawamura@gmail.com>
2020-06-12 19:23:58 -0300

    alerta de seguranca para serializacao de dados sensiveis na api

--- app/Http/Controllers/Api/Open/ProductController.php
+++ app/Http/Controllers/Api/Open/ProductController.php
@@ -2,10 +2,9 @@
 
 namespace App\Http\Controllers\Api\Open;
 
-use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Filters\Open\ProductFilter as OpenProductFilter;
-use App\Http\Resources\ProductResource;
+use App\Http\Resources\Open\ProductResource as OpenProductResource;
 use App\Models\Product;
 
 class ProductController extends Controller
@@ -15,7 +14,7 @@ class ProductController extends Controller
         $filter = app(OpenProductFilter::class);
         $filterQuery = Product::filtered($filter);
         $products = $filterQuery->where('active', true)->where('stock', '>', 0)->paginate();
-        return ProductResource::collection($products);
+        return OpenProductResource::collection($products);
     }    
     
 }
