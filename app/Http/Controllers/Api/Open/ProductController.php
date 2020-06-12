58b9a6da88dcfcda579c635b117311a4fc15922d
leonardo kawamura <leonardokawamura@gmail.com>
2020-06-12 19:36:56 -0300

    captura de um produto na area aberta da api

--- app/Http/Controllers/Api/Open/ProductController.php
+++ app/Http/Controllers/Api/Open/ProductController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api\Open;
 
 use App\Http\Controllers\Controller;
 use App\Http\Filters\Open\ProductFilter as OpenProductFilter;
+use App\Http\Resources\Open\ProductPhotoCollection as OpenProductPhotoCollection;
 use App\Http\Resources\Open\ProductResource as OpenProductResource;
 use App\Models\Product;
 
@@ -16,5 +17,17 @@ class ProductController extends Controller
         $products = $filterQuery->where('active', true)->where('stock', '>', 0)->paginate();
         return OpenProductResource::collection($products);
     }    
+
+    public function show(Product $product)
+    {
+        $product = Product::where('active', true)
+            ->where('stock', '>', 0 )
+            ->where('id', $product->id)
+            ->firstOrFail();
+
+        return new OpenProductPhotoCollection($product->photos, $product);    
+    }
+    
+    
     
 }
---
+++ app/Http/Resources/Open/ProductPhotoCollection.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Resources\Open;
+
+use App\Http\Resources\ProductPhotoResource;
+use App\Models\Product;
+use Illuminate\Http\Resources\Json\ResourceCollection;
+
+class ProductPhotoCollection extends ResourceCollection
+{
+    private $product;
+
+    public function __construct($resource, Product $product)
+    {
+        $this->product = $product;
+        parent::__construct($resource);
+    }
+
+    /**
+     * Transform the resource collection into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'product' => new ProductResource($this->product),
+            'photos' => $this->collection->map(function($photo) {
+                return new ProductPhotoResource($photo, true);
+            })
+        ];
+    }
+}
