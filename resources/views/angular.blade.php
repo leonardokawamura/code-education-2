533250870600c503c8e7ba8be3cea19745c41578
leonardo kawamura <leonardokawamura@gmail.com>
2020-06-23 12:23:18 -0300

    integracao do angular com laravel

---
+++ resources/views/angular.blade.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>Área Administrativa</title>
+        <base href="/admin">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="icon" type="image/x-icon" href="/angular/favicon.ico">
+    </head>
+    <body>
+        <app-root></app-root>
+        <script src="/angular/runtime-es2015.js" type="module"></script>
+        <script src="/angular/runtime-es5.js" nomodule defer></script>
+        <script src="/angular/polyfills-es5.js" nomodule defer></script>
+        <script src="/angular/polyfills-es2015.js" type="module"></script>
+        <script src="/angular/styles-es2015.js" type="module"></script>
+        <script src="/angular/styles-es5.js" nomodule defer></script>
+        <script src="/angular/scripts.js" defer></script>
+        <script src="/angular/vendor-es2015.js" type="module"></script>
+        <script src="/angular/vendor-es5.js" nomodule defer></script>
+        <script src="/angular/main-es2015.js" type="module"></script>
+        <script src="/angular/main-es5.js" nomodule defer></script>
+    </body>
+</html>
