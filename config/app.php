a04b96e83d929ee6f6764fc3ed8276a97d3ab5e0
leonardo kawamura <leonardokawamura@gmail.com>
2020-06-24 17:11:35 -0300

    iniciando configuracao de upload no google cloud

--- config/app.php
+++ config/app.php
@@ -150,7 +150,7 @@ return [
         /*
          * Package Service Providers...
          */
-
+        Superbalist\LaravelGoogleCloudStorage\GoogleCloudStorageServiceProvider::class,
         /*
          * Application Service Providers...
          */
